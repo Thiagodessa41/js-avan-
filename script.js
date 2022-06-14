55efdc3d3700e3b0ba4e7c07e4d1ce822c8bf350
Thiago <thiagodessa41@gmail.com>
2022-06-14 16:01:59 -0300

    second commit

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-//setInterval
+//objetos
+
+/*let pessoa ={
+    nome:'Thiago',
+    sobrenome: 'Camargo',
+    empresa: 'Novadev',
+    cargo: 'Fullstack'
+};
+
+//console.log (pessoa.nome);
+//console.log (pessoa.cargo);
 
 
-function acao(){
-    document.write('Executando....</br>')
-}
-/*
-var timer = setInterval(()=>{
-    document.write('Executando....</br>')
-},1000)
+//descontruindo objeto
+//nome:nomePessoa renomeando nome da propriedade
+const {nome:nomePessoa, cargo} = pessoa;
 */
 
 
-//setTimeout ele executa apenas uma vez
-setTimeout (()=>{
-    console.log('executou timeout!')
-}, 3000);
+
+
+// desconstruir um array
+let nomes = [
+    'Matheus',
+    'Lucas',
+    'Carlos',
+    'Pedro'
+];
+// desconstruir  passa a posicao e o nome da propriedade
+//let {0:matheus } = nomes;
+
+
+//outra maneira desconstrucao
+let [primeironome, segundonome] = nomes;
+
+console.log(primeironome);
+console.log(segundonome);
+

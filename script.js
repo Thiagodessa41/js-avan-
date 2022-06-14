0e484f43ad8a22c600e37ab9e527e77bcc33e81e
Thiago <thiagodessa41@gmail.com>
2022-06-14 18:39:33 -0300

    reduce

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-// rest Operator
-// coloque os tres pontos ira exibir todos nomes
+// map percorrer um array
 
-/*function convidados(...nomes){
-    console.log('Seja todos bem vindos todos os convida')
-    console.log(nomes);
-}
+/*let lista =['Matheus', 'jose', 'Maria'];
 
-convidados('Matheus', 'Lucas', 'Maria')*/
+lista.map((item, index)=>{
+    console.log(`Passando: ${item} - Esta na posição ${index}`)
+})*/
 
 
-function sorteador(...numeros){
-    console.log(numeros);
-    //Math.floor garante resultado numero inteiro
-    //random() numero aleatorio
-    //length tamanho do array
-    const numeroGerado = Math.floor(random() * numeros.length);
-    console.log(numeros[numeroGerado]);
-}
+//reduce reduzir um array
+/*let numeros= [5,3,2,6,8];
 
-sorteador(11,53,54,15,76,97,16)
+let total = numeros.reduce((acumulador, numero,indice, original)=>{
+    console.log(`${acumulador} -Total ate o momento`);
+    console.log(`${numero} -valor atual`);
+    console.log(`${indice} -indice atual`);
+    console.log(`${original}- array original`);
+    console.log('===========================');
+    return acumulador += numero;
+})
+
+console.log('total do reduce: ' + total);*/
